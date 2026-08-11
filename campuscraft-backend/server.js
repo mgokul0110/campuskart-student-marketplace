8c5833d5b40f114e24d4980268183f842d0321df
Apple <apple@Apples-MacBook-Pro.local>
2026-08-11 13:40:32 +0530

    Prepare application for deployment

--- campuscraft-backend/server.js
+++ campuscraft-backend/server.js
@@ -23,5 +23,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/requests", requestRoutes);
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
